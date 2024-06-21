b63dcc2b126b6cf7c134b6b9277c1d6506749f8e
“SumitCitrusbug” <sumit.citrusbug@gmail.com>
2024-06-21 11:01:38 +0530

    complted all appointment functinality

--- Patient-Management-System/app/Http/Controllers/PaymentController.php
+++ Patient-Management-System/app/Http/Controllers/PaymentController.php
@@ -3,15 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Payment;
-
+use GuzzleHttp\Psr7\Request;
 
 class PaymentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function makePayment(Request $request)
     {
-        //
     }
 }
